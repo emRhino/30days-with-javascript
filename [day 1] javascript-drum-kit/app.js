85c01f56a3f79b1724c78193e8c599634f666865
Marcin <emrhinodev@gmail.com>
2018-01-11 16:59:57 +0100

    added js which binds keys to audio files

--- [day 1] javascript-drum-kit/app.js
+++ [day 1] javascript-drum-kit/app.js
@@ -2,7 +2,23 @@
 
     'use strict';
 
+    window.addEventListener('keydown', function(e){
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+        if (!audio) return;
+
+        audio.currentTime = 0;
+        audio.play();
+        
+        key.classList.add('pressed');
+    })
+
+    function removeTransition(e) {
+        if (e.properytyname !== 'box-shadow') return;
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
 }(window));
-
-
-
